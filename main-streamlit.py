eec18588639fdc4e93ab82d28fc25fcc6ce23d20
Mike Ion <mikeion@umich.edu>
2023-03-28 11:56:55 -0400

    Update main-streamlit.py

--- main-streamlit.py
+++ main-streamlit.py
@@ -1,8 +1,3 @@
-
-
-
-
-
 import os
 import requests
 from io import BytesIO
@@ -10,9 +5,10 @@ from PyPDF2 import PdfReader
 import pandas as pd
 from openai.embeddings_utils import get_embedding, cosine_similarity
 import openai
-import pkg_resources
 import streamlit as st
 import numpy as np
+import base64
+import faiss
 
 
 
@@ -23,28 +19,48 @@ messages = [
 class Chatbot():
     
     def parse_paper(self, pdf):
+        # This function parses the PDF and returns a list of dictionaries with the text, 
+        # font size, and x and y coordinates of each text element in the PDF
         print("Parsing paper")
         number_of_pages = len(pdf.pages)
         print(f"Total number of pages: {number_of_pages}")
+        # This is the list that will contain all the text elements in the PDF and will be returned by the function
         paper_text = []
+        
         for i in range(number_of_pages):
+            # Iterate through each page in the PDF, and extract the text elements. pdf.pages is a list of Page objects.
             page = pdf.pages[i]
+            # This is the list that will contain all the text elements in the current page
             page_text = []
 
             def visitor_body(text, cm, tm, fontDict, fontSize):
+                # tm is a 6-element tuple of floats that represent a 2x3 matrix, which is the text matrix for the text.
+                # The first two elements are the horizontal and vertical scaling factors, the third and fourth elements 
+                # are the horizontal and vertical shear factors, and the fifth and sixth elements are the horizontal and vertical translation factors.
+                
+                # x and y are the coordinates of the text element
                 x = tm[4]
                 y = tm[5]
-                # ignore header/footer
+                
+                # ignore header/footer, and empty text.
+                # The y coordinate is used to filter out the header and footer of the paper
+                # The length of the text is used to filter out empty text
                 if (y > 50 and y < 720) and (len(text.strip()) > 1):
                     page_text.append({
+                    # The fontsize is used to separate paragraphs into different elements in the paper_text list
                     'fontsize': fontSize,
+                    # The text is stripped of whitespace and the \x03 character
                     'text': text.strip().replace('\x03', ''),
+                    # The x and y coordinates are used to separate paragraphs into different elements in the paper_text list
                     'x': x,
                     'y': y
                     })
 
+            # Extract the text elements from the page
             _ = page.extract_text(visitor_text=visitor_body)
+            print(f'Page {i} text", {page_text}')
 
+            
             blob_font_size = None
             blob_text = ''
             processed_text = []
@@ -71,16 +87,21 @@ class Chatbot():
                     blob_text = t['text']
                 paper_text += processed_text
         print("Done parsing paper")
-        # print(paper_text)
+        print(paper_text)
+
         return paper_text
 
     def paper_df(self, pdf):
         print('Creating dataframe')
         filtered_pdf= []
         for row in pdf:
-            if len(row['text']) < 30:
+            # This will use the get method to safely access the 'text' key in the row dictionary, 
+            # and if the key is not present, it will use an empty string as a default value. This 
+            # should prevent a KeyError from occurring.
+            if len(row.get('text', '')) < 30:
                 continue
             filtered_pdf.append(row)
+        print("Filtered paper_text", filtered_pdf)
         df = pd.DataFrame(filtered_pdf)
         print(df.shape)
         print(df.head)
@@ -94,28 +115,36 @@ class Chatbot():
         print('Calculating embeddings')
         openai.api_key = os.getenv('OPENAI_API_KEY')
         embedding_model = "text-embedding-ada-002"
-        # This is going to create embeddings for subsets of the PDF
-        embeddings = df.text.apply([lambda x: get_embedding(x, engine=embedding_model)])
-        df["embeddings"] = embeddings
-        print('Done calculating embeddings')
-        print(pkg_resources.get_distribution("openai").version)
-        return df
-    
-        
+        # Get the embeddings for each text element in the dataframe
+        embeddings = df.text.apply(lambda x: get_embedding(x, engine=embedding_model))
+        embeddings = np.vstack(embeddings, dtype=np.float32)
+        return embeddings    
 
-    def search_embeddings(self, df, query, n=3, pprint=True):
+    def search_embeddings(self, embeddings, df, query, n=3, pprint=True):
         
         # Step 1. Get an embedding for the question being asked to the PDF
-        query_embedding = get_embedding(
-            query,
-            engine="text-embedding-ada-002"
-        )
-        # Step 2. Create a column in the dataframe that contains the cosine similarity (distance) between the query and the text in the dataframe
-        df["similarity"] = df.embeddings.apply(lambda x: cosine_similarity(x, query_embedding))
-        # Step 3. Sort the dataframe by the similarity column
-        results = df.sort_values("similarity", ascending=False, ignore_index=True)
-        # make a dictionary of the the first three results with the page number as the key and the text as the value. The page number is a column in the dataframe.
-        results = results.head(n)
+        query_embedding = get_embedding(query, engine="text-embedding-ada-002")
+        query_embedding = np.array(query_embedding, dtype=np.float32)
+        # Step 2. Create a FAISS index and add the embeddings
+        d = embeddings.shape[1]
+        # Use the L2 distance metric
+        index = faiss.IndexFlatL2(d)
+        print("Embeddings shape:", embeddings.shape)
+        print("Embeddings data type:", type(embeddings))
+        index.add(embeddings)
+        
+        
+        # Step 3. Search the index for the embedding of the question
+
+        D, I = index.search(query_embedding.reshape(1,d), n)        
+        
+        # Step 4. Get the top n results from the dataframe
+        results = df.iloc[I[0]]
+        results['similarity'] = D[0]
+        results = results.reset_index(drop=True)
+        
+        # Make a dictionary of the first n results with the page number as the key and the text as the value
+        
         global sources 
         sources = []
         for i in range(n):
@@ -124,23 +153,23 @@ class Chatbot():
         print(sources)
         return results.head(n)
     
-    def create_prompt(self, df, user_input):
-        result = self.search_embeddings(df, user_input, n=3)
+    def create_prompt(self, embeddings, df, user_input):
+        result = self.search_embeddings(embeddings, df, user_input, n=3)
         print(result)
-        prompt = """You are a large language model whose expertise is reading and and providing answers about research papers. 
-        You are given a query and a series of text embeddings from a paper in order of their cosine similarity to the query.
-        You must take the given embeddings, as well as what you know from your model weights and knowledge of various fields of research to provide an answer to the query
-        that lines up with what was provided in the text.
+        prompt = """
+        You are Research Paper Guru
+        The user is going to ask you a question about a research paper after uploading a PDF of the paper.
+        You are a large language model whose expertise is reading and and providing answers to their queries, based on what you know about the subject as well as what you know about the text given to you.
             
-            Given the question: """+ user_input + """
+            The user asks: """+ user_input + """
             
-            and the following embeddings as data: 
+            And the information about the paper that is relevant to the question is: 
             
             1.""" + str(result.iloc[0]['text']) + """
             2.""" + str(result.iloc[1]['text']) + """
             3.""" + str(result.iloc[2]['text']) + """
-
-            Return a detailed answer based on the paper. If the person asks you to summarize what is in the paper, do your best to provide a summary of the paper.:"""
+            Knowing what you know about this answer, as well as being able to navigate this knowledge in conjuction with what is being said in the paper, provide an answer to the user. If the person asks you to summarize what is in the paper, do your best to provide a summary of the paper.
+            The goal here is to keep the user happy and satisfied that you have given them the best answer to the question to the best of your knowledge. If necessary, you can also point them to outside resources for more information.:"""
 
         print('Done creating prompt')
         return prompt
@@ -154,9 +183,9 @@ class Chatbot():
         response = {'answer': answer, 'sources': sources}
         return response
 
-    def reply(self, prompt):
-        print(prompt)
-        prompt = self.create_prompt(df, prompt)
+    def reply(self, embeddings, user_input):
+        print(user_input)
+        prompt = self.create_prompt(embeddings, df, user_input)
         return self.gpt(prompt)
 
 def process_pdf(file):
@@ -166,8 +195,9 @@ def process_pdf(file):
     paper_text = chatbot.parse_paper(pdf)
     global df
     df = chatbot.paper_df(paper_text)
-    df = chatbot.calculate_embeddings(df)
+    embeddings = chatbot.calculate_embeddings(df)
     print("Done processing pdf")
+    return embeddings
 
 def download_pdf(url):
     chatbot = Chatbot()
@@ -177,8 +207,9 @@ def download_pdf(url):
     paper_text = chatbot.parse_paper(pdf)
     global df
     df = chatbot.paper_df(paper_text)
-    df = chatbot.calculate_embeddings(df)
+    embeddings = chatbot.calculate_embeddings(df)
     print("Done processing pdf")
+    return embeddings
 
 def show_pdf(file_content):
     base64_pdf = base64.b64encode(file_content).decode('utf-8')
@@ -189,7 +220,7 @@ def show_pdf(file_content):
 def main():
     st.title("Research Paper Guru")
     st.subheader("Upload PDF or Enter URL")
-    
+    embeddings = None
     pdf_option = st.selectbox("Choose an option:", ["Upload PDF", "Enter URL"])
     chatbot = Chatbot()
 
@@ -197,7 +228,7 @@ def main():
         uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
         if uploaded_file is not None:
             file_content = uploaded_file.read()
-            process_pdf(uploaded_file.read())
+            embeddings = process_pdf(file_content)
             st.success("PDF uploaded and processed successfully!")
             show_pdf(file_content)
 
@@ -208,16 +239,19 @@ def main():
                 try:
                     r = requests.get(str(url))
                     content = r.content
-                    download_pdf(url)
+                    embeddings = download_pdf(url)
                     st.success("PDF downloaded and processed successfully!")
                     show_pdf(content)
                 except Exception as e:
                     st.error(f"An error occurred while processing the PDF: {e}")
-
+    st.subheader("Ask a question about a research paper and get an answer with sources!")
     query = st.text_input("Enter your query:")
     if query:
         if st.button("Get answer"):
-            response = chatbot.reply(query)
+            if embeddings is not None:
+                response = chatbot.reply(embeddings, query)
+            else:
+                st.warning("Please upload a PDF or enter a URL first.")
             st.write(response['answer'])
             st.write("Sources:")
             for source in response['sources']:
